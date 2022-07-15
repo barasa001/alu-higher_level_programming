4bb56aed0ffac485fceec0dd9d93bdb627901793
root <s.wanyonyi@alustudent.com>
2022-07-14 22:22:32 -0700

    New commit

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 
+
 def no_c(my_string):
-    str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            str = str + i
-    return str
+    size = len(my_string)
+    j = 0
+    new_sTr = my_string[:]
+    for i in range(size):
+        if (my_string[i] == 'c'or my_string[i] == 'C'):
+            new_sTr = new_sTr[:(i - j)] + my_string[(i + j):]
+            j += 1
+    return (new_sTr)
