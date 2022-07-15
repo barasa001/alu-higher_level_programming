04266d1df6cce1b0aea806150899c01123c4fd21
root <s.wanyonyi@alustudent.com>
2022-07-14 20:43:42 -0700

    New commit

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,5 +5,5 @@ def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return
     my_list.reverse()
-    fir i in my_list:
+    for i in my_list:
         print("{:d}".format(i))
