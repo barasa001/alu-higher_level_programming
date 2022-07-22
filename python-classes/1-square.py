884e5a353058698099e2be2480601d620095f56b
root <s.wanyonyi@alustudent.com>
2022-07-22 05:05:00 -0700

    First commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Writing class square to define size.
+"""
+
+class Square:
+    """Class Square that defines a square object
+    """
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
+
+        Args:
+            param1 (int): size of the square
+        """
+        self.__size = size
