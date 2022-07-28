2da4a429cacb62a0c3c3d5f14bcd76edc2fbfe3d
root <s.wanyonyi@alustudent.com>
2022-07-28 15:36:38 -0700

    The commit

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -8,4 +8,4 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """ Class that defines a rectangle from BaseGeometry Class """
 
-    def __init__(self, width, height):
+
