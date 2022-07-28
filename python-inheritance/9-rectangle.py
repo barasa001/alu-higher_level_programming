f3f2c781793306f40b04f29c5217e64b2e134676
root <s.wanyonyi@alustudent.com>
2022-07-28 14:33:47 -0700

    This is for the change

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,15 +1,2 @@
 #!/usr/bin/python3
 """The module for import"""
-
-
-class Square(Rectangle):
-    """ Class that defines a Square from Rectangle class """
-    def __init__(self, size):
-        """ Method that initializes a Square """
-        self.integer_validator("size", size)
-        self.__size = size
-        super().__init__(self.__size, self.__size)
-
-    def area(self):
-        """ Method that returns a string with the area """
-        return super().area()
