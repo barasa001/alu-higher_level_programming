64e6c32cf6f5a8eb666dcce800e1d5d1547dc8a6
root <s.wanyonyi@alustudent.com>
2022-07-28 12:53:28 -0700

    Other commit

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is the module for attributes and classes"""
+
+class MyList(list):
+    """ Class that inherits the attributes references of class list
+
+    Args:
+        list: class list
+
+    """
+
+    def print_sorted(self):
+        """ Method that prints the sorted list """
+        l_sorted = self.copy()
+        l_sorted.sort()
+        print(l_sorted)
