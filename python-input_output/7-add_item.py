b3457635e2a00c709e5dc39096d7dde255c1fb33
root <s.wanyonyi@alustudent.com>
2022-07-28 18:39:11 -0700

    This works

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,20 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then saves them to a file
-"""
+""" File name : 9-add_item.py"""
+import sys
 
-from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-
-filename = "add_item.json"
-
-try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+if __name__ == "__main__":
