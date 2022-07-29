6b974c8b1dd8a786d52fa5f5740041231c56e685
root <s.wanyonyi@alustudent.com>
2022-07-28 18:26:44 -0700

    Other commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,9 +3,15 @@
 save them to a file
 """
 import sys
-import os.path
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+            __import__('8-load_from_json_file').load_from_json_file
 
-"""The module"""
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
