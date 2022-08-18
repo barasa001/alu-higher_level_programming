39be00639faf66e3e4b23dabffea321cd386c88a
root <s.wanyonyi@alustudent.com>
2022-08-17 21:31:02 -0700

    Other commit

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,3 +8,6 @@ of hbtn_0e_0_usa where name matches the argument
 if __name__ == "__main__":
     from sys import argv
     import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
