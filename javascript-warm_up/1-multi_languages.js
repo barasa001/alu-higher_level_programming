91b55e7e84417b7f7afb2f94f82d9ab7e0c8bab0
barasa001 <s.wanyonyi@alustudent.com>
2022-10-04 18:00:19 +0000

    New commit

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const myCone = 'C is fun'
-const myJone = 'Python is cool'
-const myPone = 'JavaScript is amazing'
-console.log(myCone)
-console.log(myJone)
-console.log(myPone)
+const myCone = 'C is fun';
+const myJone = 'Python is cool';
+const myPone = 'JavaScript is amazing';
+console.log(myCone);
+console.log(myJone);
+console.log(myPone);
