4ba65e685c9803374429f9c1935fee0a73687c5e
barasa001 <s.wanyonyi@alustudent.com>
2022-10-09 09:07:18 +0000

    The correction

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const args = process.argv;
-if (args[2] != null); {
-  console.log(args[2]);
-} else {
+if (process.argv[2] === undefined) {
   console.log('No argument');
+} else {
+  console.log(process.argv[2]);
 }
