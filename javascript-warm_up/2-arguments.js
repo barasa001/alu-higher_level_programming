2eb30aaa3c89fb0043c0877db8de949698e715fd
barasa001 <s.wanyonyi@alustudent.com>
2022-10-09 07:52:22 +0000

    The commit

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,5 +2,5 @@
 if (process.argv.length < 3){
 console.log('No argument');}
 else if (process.argv.length ===3){
-	console.log ('Argument found');}
-else {console.log ('Argument found');}
+	console.log ('Arguments found');}
+else {console.log ('Arguments found');}
