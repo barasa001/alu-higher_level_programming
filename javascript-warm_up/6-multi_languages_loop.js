2bd871a99a9203c95572680bc99a1d95011c04c1
barasa001 <s.wanyonyi@alustudent.com>
2022-10-10 04:34:50 +0000

    The correction

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const giveLine = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const giveLine = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(giveLine[i]);
 }
