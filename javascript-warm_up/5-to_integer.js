5474ae497c79ff56f5b4d4c62549ce370d992f9a
barasa001 <s.wanyonyi@alustudent.com>
2022-10-10 04:02:43 +0000

    The commit

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const newInt = parseInt(process.argv[2]);
-if (newInt.isNaN(newInt)) {
+if (Number.isNaN(newInt)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + process.argv[2]);
