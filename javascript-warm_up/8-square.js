5205fcc9c45d25989dfb48c24dcb6cb6e9442027
barasa001 <s.wanyonyi@alustudent.com>
2022-10-10 05:04:48 +0000

    The commit

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-const theSquare = parseInt(process.argv[2]);
-if (Number.isNaN(theSquare)) {
+const size = parseInt(process.argv[2]);
+if (Number.isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0, s; i < theSquare; i++) {
+  for (let i = 0, s; i < size; i++) {
     s = '';
-    for (let j = 0; j < theSquare; j++) {
+    for (let j = 0; j < size; j++) {
       s += 'X';
     }
     console.log(s);
+  }
 }
