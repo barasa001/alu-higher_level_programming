d5dcf5674c2f1a1d5147dfa1c6bf99eaaf58284f
barasa001 <s.wanyonyi@alustudent.com>
2022-10-12 17:55:09 +0000

    The commit

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
+const args = process.argv;
+
+request(`https://swapi-api.hbtn.io/api/films/${args[2]}`, function (error, response, body) {
+  if (error) {
+    console.log(error);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log(JSON.parse(body).title);
   }
 });
