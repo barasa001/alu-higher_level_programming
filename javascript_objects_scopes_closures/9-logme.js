18bd2c28e19188f3a919c65a263a773d7b5dd925
barasa001 <s.wanyonyi@alustudent.com>
2022-10-12 14:46:54 +0000

    The new commit

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-let allArg = 0;
-
+let count = 0;
 exports.logMe = function (item) {
-  console.log(allArg + ': ' + item;
-  allArg++;
+  console.log(count + ': ' + item);
+  count++;
 };
